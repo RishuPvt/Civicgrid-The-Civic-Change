93f19ea05b0ddce03319966af14a079bf6ae27c1
Rishu Raj <rishuraj1127@gmail.com>
2025-08-20 10:48:09 +0530

    First commit

--- Backend/App.js
+++ Backend/App.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const corsOptions = {
-  origin: "https://backend-hub.vercel.app",
+  origin: "http://localhost:5173/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
@@ -19,5 +19,8 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
+import userRouter from "./src/Routes/User.Route.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
---
+++ Backend/src/Routes/User.Route.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {   RegisterUser,
+  LogInUser,
+  LogoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar, } from "../Controller/User.controller.js";
+
+  import {verifyJWT} from "../Middleware/Auth.middleware.js"
+  import {upload} from "../Middleware/Multer.js"
+
+
+
+  const router = Router();
+
+  router.route("/RegisterUser").post(RegisterUser)
+   router.route("/LogInUser").post(LogInUser)
+   router.route("/LogoutUser").post(verifyJWT ,LogoutUser)
+   router.route("/changeCurrentPassword").post(verifyJWT ,changeCurrentPassword)
+   router.route("/getCurrentUser").get(verifyJWT ,getCurrentUser)
+   router.route("/updateAccountDetails").patch(verifyJWT ,updateAccountDetails)
+   router.route("/updateUserAvatar").put(verifyJWT, upload.single("avatar") ,updateUserAvatar)
+
+
+
+
+  export default router;
