73b85eb269171e0219d40db6f501f41095685ce4
Rishu Raj <rishuraj1127@gmail.com>
2025-09-15 19:17:52 +0530

    New task router

--- Backend/src/Routes/Task.Router.js
+++ Backend/src/Routes/Task.Router.js
@@ -10,13 +10,14 @@ import {
   GetTasksbyUser,
   AddComment,
   VoteonTask,
+  getTaskDetails
 } from "../Controller/Task.controller.js";
 
 const router = Router();
 
 router
   .route("/CreateTask")
-  .post(verifyJWT, upload.single("imageUrl"), CreateTask);
+  .post(verifyJWT, upload.array("imageUrl"), CreateTask);
 
 router.route("/GetAllTasks").get(verifyJWT, GetAllTasks);
 
@@ -26,8 +27,11 @@ router.route("/Badge/updateCivicScore").patch(verifyJWT, updateCivicScore);
 router.route("/GetTasksbyUser").get(verifyJWT, GetTasksbyUser);
 
 
-router.route("/AddComment/:taskId").patch(verifyJWT, AddComment);
+router.route("/AddComment/:taskId").post(verifyJWT, AddComment);
 
-router.route("/VoteonTask/:taskId").patch(verifyJWT, VoteonTask);
+router.route("/VoteonTask/:taskId").post(verifyJWT, VoteonTask);
+
+router.route("/getTaskDetails/:taskId").get(  getTaskDetails);
+
 
 export default router;
