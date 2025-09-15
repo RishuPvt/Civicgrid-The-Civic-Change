c32885f51da3481422daae1e93e0e8dc4f026aa7
Rishu Raj <rishuraj1127@gmail.com>
2025-09-15 19:21:43 +0530

    Updated app.js

--- Backend/App.js
+++ Backend/App.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const corsOptions = {
-  origin: "http://localhost:5173/",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
